30cf995f7808dfc9f2c4a7a23bf6675a34cf01c3
1fatimakhan <140249722+1fatimakhan@users.noreply.github.com>
2023-07-23 10:57:21 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # GitHub username
    # Input:
    # p, principal amount
    # t, time period in years
